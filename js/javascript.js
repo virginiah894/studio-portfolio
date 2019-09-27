509df8b3a7f119775fe9e6229e7b33cd4ee3eab5
virginiah894 <virgyperry@gmail.com>
2019-09-28 00:11:59 +0300

    add toggle effect on icons using jquery

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function() {
+  $("#design").click(function() {
+    $("#des").toggle();
+  });
+});
+
+$("#dev").click(function() {
+  $("#desA").toggle();
+});
+$("#prd").click(function() {
+  $("#desB").toggle();
+});
+
