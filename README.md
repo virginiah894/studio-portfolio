a2f7fbf8594c8d35e0ce30dcd97aaf13278a2063
virginiah894 <virgyperry@gmail.com>
2019-09-28 22:00:26 +0300

    add README.md file

---
+++ README.md
@@ -0,0 +1,32 @@
+## Weebsite Name
+Akan Naming applicatio website.
+
+Brief description of application
+This is a website application which integrates the user's input which is date of birth,Month of birth,Year of birth and the century.The app calculates finds the day of the week using the formula Day of the week (d) = ( ( (CC/4) -2CC-1) + ((5YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7. The value acquired is what the system uses to give you an Akan name from Ghana.
+
+List of contributors
+Virginiah Periah github:virginiah@894
+
+About author
+Virginiah periah is a student at moringa school studying Software Engineering.
+
+BDD
+(Behavior Driven Development) This application uses javascript because the language is very lightweight and makes the calculations fast and interactive.
+
+Setup/Installation Requirements
+The application is simple to use. Here are instructions
+
+Enter your date of birth correctly
+Input the month of the birth
+Input the year and the century
+click submit and your "Akan Name" will be shown on the screen.
+Technologies Used
+The application uses English Language. It uses HTML ,css,javascript and bootstrap .
+
+Support and contact details
+For any queries, ideas on improvement,requests for contribution ,feel free to email me on virgyperry@gmail.com
+
+License
+Copyright (c) Virginiah Periah 2019 Licensed under the Apache License, Version 3.0 (the "License");
+
+© 2019 GitHub, Inc.
--- js/javascript.js
+++ js/javascript.js
@@ -14,3 +14,45 @@ $("#prd").click(function() {
 $("#contacts").css("color","white");
 $(".data").css("background-color","transparent");
 $("#msg").css("height","200px");
+
+$(".data").submit(function(event) {
+  var forms= ["#name", "#email","#msg"];
+
+  forms.forEach(function(form) {
+    var userInput = $(".data" + blank).val();
+    $("." + blank).text(userInput);
+  });
+$("#contacts").text(alert)
+
+
+  event.preventDefault();
+});
+
+// $("#one").mouseover(function() {
+//  $("#one").text("Project one").css("background-color","blue", "position","absolute");
+// });
+
+// $("#one").hover(function(){
+//   $("#one").text("Project one").css("background-color","blue", "overflow","hidden");
+//   });
+//   $("#two").hover(function(){
+//     $("#two").text("Project one") .css("background-color","white");
+//     });
+//     $("#three").hover(function(){
+//   $("#three").addClass("ma").text("Project one");
+//   });
+//   $("#four").hover(function(){
+//     $("#four").addClass("ma").text("Project one");
+//     });
+//     $("#five").hover(function(){
+//       $("#five").addClass("ma").text("Project one");
+//       });
+//       $("#six").hover(function(){
+//         $("#six").addClass("ma").text("Project one");
+//         });
+//         $("#seven").hover(function(){
+//           $("#seven").addClass("ma").text("Project one");
+//           });
+//           $("#eight").hover(function(){
+//             $("#eight").addClass("ma").text("Project one");
+//             });
