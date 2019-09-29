a47da8365e77348f8eda42b886adade641996ea9
virginiah894 <virgyperry@gmail.com>
2019-09-29 18:10:37 +0300

    add alerts on the form

--- js/javascript.js
+++ js/javascript.js
@@ -90,5 +90,15 @@ $("#one").mouseleave(function(){
                  $("#sub").submit(function one (){
                   alert("Submitted");
                 });
-              $("#sub").hide()
-              
+              $("#sub").show()
+          
+              function alertFunction(){
+                var test = "test";
+                var email = "email@domain.com"
+                var message = "a message"
+                test = document.getElementById("name").value;
+                email = document.getElementById("email").value;
+                message = document.getElementById("msg").value;
+            
+                alert("Thank you " + test + " for contacting us  We have received your message ");
+              }
